f0566d6a312d6875dbe126cb9e69afe5b84afab7
Gnonymous <gnonymous.ai@gmail.com>
2024-12-13 15:26:09 +0800

    Update

---
+++ run.sh
@@ -0,0 +1,6 @@
+
+
+image_dir="imgs/"
+
+python test_video_thread.py "${image_dir}" &
+python test_client.py
--- server/server.py
+++ server/server.py
@@ -4,8 +4,6 @@ import sys
 
 sys.path.append('./')
 sys.path.append('../')
-import video.call_vl_model
-import audio.call_asr_model
 from video.vl_model import VlModel
 
 # 这里只是占位，实际中需要替换为真正处理视频信息的相关逻辑和模型调用等
@@ -28,9 +26,9 @@ class VideoInfoHandler(socketserver.BaseRequestHandler):
 # 处理图文问答相关服务的类（实际需对接对应模型或接口等）
 class ImageTextQAHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        v_model = VlModel
         while True:
             try:
+                model = VlModel()
                 # 省略语音内容转asr步骤，假设得到文本开始处理
                 data = self.request.recv(1024).decode('utf-8')
                 # TODO 处理VAD结果，通过ASR转文本
@@ -56,7 +54,7 @@ class ImageTextQAHandler(socketserver.BaseRequestHandler):
                 # response = '这是模拟图文问答的回复'
 
                 # 本地调用demo。modelscope
-                response = v_model.video_chat(data, VIDEO_INFO)
+                response = model.video_chat(data, VIDEO_INFO)
                 info_log = f"用户输入:{data},当前视频长度:{len(VIDEO_INFO)},模型回复:{response}"
                 print(info_log)
                  # 修改部分：检查 response 类型并转化为字符串
